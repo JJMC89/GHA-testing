fd7980523eb5c1c62eee6fa4e13ae5f5b5d8b3e2
JJ <JJMC89@users.noreply.github.com>
2020-08-17 09:20:34 -0700

    Update app.py

--- src/pkg/app.py
+++ src/pkg/app.py
@@ -2,5 +2,11 @@
 import argparse
 
 def main():
-  """Dummy."""
-  parser = argparse.ArgumentParser(description="This")
+    """Dummy."""
+    parser = argparse.ArgumentParser(description="This")
+    subparsers = parser.add_subparsers(dest="action", required=True)
+    subparsers.add_parser(
+        "reduce-tagger",
+        parents=[always_parser, summary_parser],
+        help="tag files for size reduction",
+    )
