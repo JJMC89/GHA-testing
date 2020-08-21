40a44a2ba94d44cadc224492cf1420d42c0e1462
JJ <JJMC89@users.noreply.github.com>
2020-08-20 18:19:42 -0700

    GH stuff

---
+++ src/pkg/app.py
@@ -0,0 +1,9 @@
+"""This is a dummy app."""
+import argparse
+
+
+def main():
+    """Run."""
+    parser = argparse.ArgumentParser(description="This")
+    subparsers = parser.add_subparsers(dest="action", required=True)
+    subparsers.add_parser("reduce-tagger", help="tag files for size reduction")
